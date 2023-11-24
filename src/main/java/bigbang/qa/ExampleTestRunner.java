05f7d87fcc8efb2ebcbad0d9ede99a7610bbf431
aadavydkin <AADavydkin@mtsbank.ru>
2023-11-24 10:09:37 +0300

    initial commit

--- src/main/java/bigbang/qa/ExampleTestRunner.java
+++ src/main/java/bigbang/qa/ExampleTestRunner.java
@@ -4,15 +4,16 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-// , plugin =
-//         {"ru.testit.listener.BaseCucumber7Listener",
-//         "progress",
-//         "summary"}
-//
+
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources"
         , glue = "bigbang.qa.steps"
        , tags = "@tag1"
-        )
+        , plugin =
+        {"ru.testit.listener.BaseCucumber7Listener",
+                "progress",
+                "summary"}
+
+)
 public class ExampleTestRunner {
 }
